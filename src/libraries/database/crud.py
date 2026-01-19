8c3a9ac0a94e38cedb52b1de195651b34d157604
Humonia SYS <charass339@gmail.com>
2026-01-19 22:22:02 +0800

    🐛 fix(crud): handle ValueError when converting user_id for ONEBOT_V11 platform

--- src/libraries/database/crud.py
+++ src/libraries/database/crud.py
@@ -12,10 +12,11 @@ async def get_user_by_platform_id(
     session: AsyncSession, platform: PlatfromType, user_id: str | int
 ) -> Optional[User]:
     stmt = select(User)
-
     if platform == PlatfromType.ONEBOT_V11:
-        stmt = stmt.where(User.qid == int(user_id))
-
+        try:
+            stmt = stmt.where(User.qid == int(user_id))
+        except ValueError:
+            return None
     elif platform == PlatfromType.QQ_DIRECT:
         stmt = stmt.where(User.user_openid == str(user_id))
     elif platform == PlatfromType.QQ_GROUP:
@@ -27,10 +28,9 @@ async def get_user_by_platform_id(
 
     return result.scalar_one_or_none()
 
+
 async def create_user(
-        session: AsyncSession,
-        platform: PlatfromType,
-        user_id: str | int
+    session: AsyncSession, platform: PlatfromType, user_id: str | int
 ) -> User:
     new_user = User()
 
